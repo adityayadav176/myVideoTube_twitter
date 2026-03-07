eeca914f9bc979a50c9b2f08f1f0f2809f2745c8
adityayadav176 <ay6598aditya@gmail.com>
2026-03-07 21:28:17 +0530

    add new backend project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import ConnectedToMongo from "./db/db.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 import express from "express"
 import dotenv from "dotenv"
  dotenv.config({
     path: './env'
  })
 const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 app.listen(process.env.PORT, ()=>{
     console.log(`App is running is  port ${process.env.PORT}`);
---
+++ src/models/video.models.js
@@ -0,0 +1,42 @@
+import mongoose, { Mongoose } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const { Schema } = mongoose
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,  // cloudinary url
+        required: true
+    },
+    thumbnail: {
+        type: String,  // cloudinary url
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number, // cloudinary url  
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+videoSchema.plugin(mongooseAggregatePaginate)
+const Video = mongoose.model("Video", videoSchema)
+module.exports = Video
